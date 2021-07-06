635f0b7684328c2ab3e31e44a7e9c0c444912e23
BuildTools <maciekpeczyk@gmail.com>
2021-07-06 16:38:42 +0200

    prosty config i ochrona spawnu

--- src/main/java/pl/pivipi/core/Core.java
+++ src/main/java/pl/pivipi/core/Core.java
@@ -1,15 +1,27 @@
 package pl.pivipi.core;
 
+import java.io.File;
+
+import org.bukkit.configuration.file.FileConfiguration;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
 import pl.pivipi.core.commands.CmdIp;
 import pl.pivipi.core.commands.CmdStat;
 import pl.pivipi.core.commands.CmdZakup;
+import pl.pivipi.core.events.SpawnProtection;
 
 public class Core extends JavaPlugin {
 	
+	private File configYml = new File(getDataFolder(), "config.yml");
+	public FileConfiguration configCfg = YamlConfiguration.loadConfiguration(configYml);
+	
 	@Override
 	public void onEnable() {
+		if (!(configYml.exists())) saveResource("config.yml", false);
+		if (configCfg.getBoolean("modules.better_spawn_protection.enable")) {
+			getServer().getPluginManager().registerEvents(new SpawnProtection(this), this);
+		}
 		getCommand("zakup").setExecutor(new CmdZakup());
 		getCommand("stat").setExecutor(new CmdStat());
 		getCommand("ip").setExecutor(new CmdIp());
---
+++ src/main/java/pl/pivipi/core/events/SpawnProtection.java
@@ -0,0 +1,95 @@
+package pl.pivipi.core.events;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
+import org.bukkit.entity.Player;
+import org.bukkit.event.EventHandler;
+import org.bukkit.event.Listener;
+import org.bukkit.event.block.BlockBreakEvent;
+import org.bukkit.event.block.BlockExplodeEvent;
+import org.bukkit.event.block.BlockIgniteEvent;
+import org.bukkit.event.block.BlockPlaceEvent;
+import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.entity.EntityDamageEvent;
+import org.bukkit.event.entity.EntitySpawnEvent;
+
+import pl.pivipi.core.Core;
+
+public class SpawnProtection implements Listener {
+	private Core plugin;
+	
+	public SpawnProtection(Core plugin) {
+		this.plugin = plugin;
+	}
+	
+	int radius = plugin.configCfg.getInt("modules.better_spawn_protection.size");
+	Location center = Bukkit.getWorld("world").getSpawnLocation();
+	
+	private boolean isInSpawn(Location lok) {
+		if (lok.getWorld() == center.getWorld()) {
+			if (lok.getX() <= center.getX() + radius && lok.getX() >= center.getX() - radius) {
+				if (lok.getZ() <= center.getZ() + radius && lok.getZ() >= center.getZ() - radius) {
+					return true;
+				}
+			}
+		}
+		return false;
+	}
+	
+	@EventHandler
+	public void bsExplode(BlockExplodeEvent e) {
+		if (isInSpawn(e.getBlock().getLocation())) {
+			e.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void bsBreak(BlockBreakEvent e) {
+		if (!(e.getPlayer().hasPermission("core.bypasssp"))) {
+			if (isInSpawn(e.getBlock().getLocation())) {
+				e.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void bsPlace(BlockPlaceEvent e) {
+		if (!(e.getPlayer().hasPermission("core.bypasssp"))) {
+			if (isInSpawn(e.getBlock().getLocation())) {
+				e.setCancelled(true);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void bsFire(BlockIgniteEvent e) {
+		if (isInSpawn(e.getBlock().getLocation())) {
+			e.setCancelled(true);
+		}
+		if (e.getIgnitingEntity() != null) {
+			if (e.getPlayer().hasPermission("core.bypasssp")) {
+				e.setCancelled(false);
+			}
+		}
+	}
+	
+	@EventHandler
+	public void bsSpawn(EntitySpawnEvent e) {
+		if (isInSpawn(e.getLocation())) {
+			e.setCancelled(true);
+		}
+	}
+	
+	@EventHandler
+	public void bsDamage(EntityDamageEvent e) {
+	    EntityDamageByEntityEvent ee = (EntityDamageByEntityEvent) e;
+		if (e.getEntity() instanceof Player && isInSpawn(e.getEntity().getLocation())) {
+			e.setCancelled(true);
+		    if (ee.getDamager() instanceof Player) {
+		    	if (((Player)ee.getDamager()).hasPermission("core.bypasssp")) {
+		    		e.setCancelled(false);
+		    	}
+		    }
+		}
+	}
+}
