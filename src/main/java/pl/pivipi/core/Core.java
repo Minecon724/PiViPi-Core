66b1f49ce58783255f7dc6d28366192f38175106
BuildTools <maciekpeczyk@gmail.com>
2021-07-10 14:23:18 +0200

    serwer http (pewnie nie zadziala)

--- src/main/java/pl/pivipi/core/Core.java
+++ src/main/java/pl/pivipi/core/Core.java
@@ -1,6 +1,8 @@
 package pl.pivipi.core;
 
 import java.io.File;
+import java.io.IOException;
+import java.net.InetSocketAddress;
 import java.util.logging.Level;
 
 import org.bukkit.Bukkit;
@@ -8,12 +10,15 @@ import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.plugin.java.JavaPlugin;
 
+import com.sun.net.httpserver.HttpServer;
+
 import pl.pivipi.core.commands.CmdCore;
 import pl.pivipi.core.commands.CmdIp;
 import pl.pivipi.core.commands.CmdStat;
 import pl.pivipi.core.commands.CmdZakup;
 import pl.pivipi.core.events.Chat;
 import pl.pivipi.core.events.SpawnProtection;
+import pl.pivipi.core.webapi.WebHandler;
 
 public class Core extends JavaPlugin {
 	
@@ -23,6 +28,10 @@ public class Core extends JavaPlugin {
 	@Override
 	public void onEnable() {
 		if (!(configYml.exists())) saveResource("config.yml", false);
+		getCommand("zakup").setExecutor(new CmdZakup());
+		getCommand("stat").setExecutor(new CmdStat());
+		getCommand("ip").setExecutor(new CmdIp());
+		getCommand("core").setExecutor(new CmdCore(this));
 		if (configCfg.getBoolean("modules.better_spawn_protection.enable")) {
 			Bukkit.getLogger().log(Level.INFO, "Enabling better spawn protection module...");
 			getServer().getPluginManager().registerEvents(new SpawnProtection(this), this);
@@ -31,11 +40,19 @@ public class Core extends JavaPlugin {
 			Bukkit.getLogger().log(Level.INFO, "Enabling chat module...");
 			getServer().getPluginManager().registerEvents(new Chat(this), this);
 		}
-		Bukkit.getLogger().log(Level.INFO, "Enabling other modules...");
-		getCommand("zakup").setExecutor(new CmdZakup());
-		getCommand("stat").setExecutor(new CmdStat());
-		getCommand("ip").setExecutor(new CmdIp());
-		getCommand("core").setExecutor(new CmdCore(this));
+		if (configCfg.getBoolean("modules.webapi.enable")) {
+			Bukkit.getLogger().log(Level.INFO, "Starting WebApi...");
+			HttpServer server;
+			try {
+				server = HttpServer.create(new InetSocketAddress(configCfg.getInt("modules.webapi.port")), 0);
+				server.createContext("/", new WebHandler());
+			    server.setExecutor(null);
+			    server.start();
+			} catch (IOException e) {
+				e.printStackTrace();
+				Bukkit.getLogger().log(Level.SEVERE, "Failed to start WebApi");
+			}
+		}
 		Bukkit.getLogger().log(Level.INFO, getDescription().getName() + " v" + getDescription().getVersion() + " loaded!");
 	}
 	
---
+++ src/main/java/pl/pivipi/core/webapi/WebHandler.java
@@ -0,0 +1,54 @@
+package pl.pivipi.core.webapi;
+
+import java.io.IOException;
+import java.io.OutputStream;
+
+import org.bukkit.Bukkit;
+import org.bukkit.Material;
+import org.bukkit.Statistic;
+import org.bukkit.block.Block;
+import org.bukkit.craftbukkit.libs.org.apache.commons.lang3.EnumUtils;
+import org.bukkit.entity.EntityType;
+import org.bukkit.entity.Player;
+
+import com.sun.net.httpserver.HttpExchange;
+import com.sun.net.httpserver.HttpHandler;
+
+public class WebHandler implements HttpHandler {
+	public void handle(HttpExchange t) throws IOException {
+		String uri = t.getRequestURI().toString().substring(1);
+		String[] parts = uri.split("/");
+		Player player = Bukkit.getPlayer(parts[0]);
+		int rCode = 200;
+		String response = "OK";
+		if (player == null) {
+			rCode = 404;
+			response = "Player " + parts[0] + " not found";
+		} else {
+			try {
+				if (EnumUtils.isValidEnum(Statistic.class, parts[1].toUpperCase())) {
+					int statistic;
+					if (EnumUtils.isValidEnum(EntityType.class, parts[2].toUpperCase())) {
+						statistic = player.getStatistic(Statistic.valueOf(parts[1].toUpperCase()), EntityType.valueOf(parts[2].toUpperCase()));
+					} else if (EnumUtils.isValidEnum(Material.class, parts[2].toUpperCase())) {
+						statistic = player.getStatistic(Statistic.valueOf(parts[1].toUpperCase()), Material.valueOf(parts[2].toUpperCase()));
+					} else {
+						statistic = player.getStatistic(Statistic.valueOf(parts[1].toUpperCase()));
+					}
+					rCode = 200;
+					response = Integer.toString(statistic);
+				} else {
+					rCode = 404;
+					response = "Statistic " + parts[1].toUpperCase() + " not found";
+				}
+			} catch (IllegalArgumentException e) {
+				rCode = 406;
+				response = e.getMessage();
+			}
+		}
+		t.sendResponseHeaders(rCode, response.length());
+		OutputStream os = t.getResponseBody();
+		os.write(response.getBytes());
+		os.close();
+	}
+}
